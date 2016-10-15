2bcb9d56eb46335b14e8159b7425f8927c4dd9e6
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-10-15 13:49:10 +0530

    Swapping Screens Init

---
+++ SwappingScreens/SwappingScreens/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Brijrajsinh Gohil on 15/10/16.
+//  Copyright © 2016 Brijrajsinh Gohil. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
